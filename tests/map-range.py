b2dab4ec2342e59f8404f252fe7ac998d90b25a8
Dan Smith <dan@bytesandbolts.com>
2015-10-23 15:25:51 +0100

    Added map-range example

---
+++ tests/map-range.py
@@ -0,0 +1,17 @@
+#!/Users/dan/.virtualenvs/instagram-test/bin/python
+
+# Sourced from: http://stackoverflow.com/questions/1969240/mapping-a-range-of-values-to-another
+def translate(value, leftMin, leftMax, rightMin, rightMax):
+    # Figure out how 'wide' each range is
+    leftSpan = leftMax - leftMin
+    rightSpan = rightMax - rightMin
+
+    # Convert the left range into a 0-1 range (float)
+    valueScaled = float(value - leftMin) / float(leftSpan)
+
+    # Convert the 0-1 range into a value in the right range.
+    return rightMin + (valueScaled * rightSpan)
+
+sensor_value = 256
+actuator_value = translate(sensor_value, 1, 512, 5, 10)
+print actuator_value
