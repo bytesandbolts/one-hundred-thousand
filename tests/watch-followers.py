d8c941fcb215d1d63482979bfa653dac5abab2b7
Dan Smith <dan@bytesandbolts.com>
2015-10-23 13:16:07 +0100

    Polling working correctly with timestamp

--- tests/watch-followers.py
+++ tests/watch-followers.py
@@ -6,11 +6,15 @@ access_token = "176201887.877f884.1cdbffd746974bb489a66c2b1df0e958"
 client_secret = "e8336639f78444919b8b3f4c1ef0c326"
 dan_user_id = "176201887"
 laura_user_id = "201990584"
+previous_followers = 0
 
 api = InstagramAPI(access_token=access_token, client_secret=client_secret)
 
 while True:
     user_info = api.user(user_id=laura_user_id)
     number_of_followers = user_info.counts["followed_by"]
-    print number_of_followers
+    if number_of_followers != previous_followers:
+        print "Followers: %s - %s" % (number_of_followers, time.asctime(time.localtime(time.time())))
+        previous_followers = number_of_followers
+
     time.sleep(1)
