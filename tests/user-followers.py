f5e78d4e33b9133d07b8f0021141e473fbeee768
Dan Smith <dan@bytesandbolts.com>
2015-10-23 12:32:45 +0100

    Adder user specific id's

--- tests/user-followers.py
+++ tests/user-followers.py
@@ -3,9 +3,10 @@ import json
 
 access_token = "176201887.877f884.1cdbffd746974bb489a66c2b1df0e958"
 client_secret = "e8336639f78444919b8b3f4c1ef0c326"
-user_id = "176201887"
+dan_user_id = "176201887"
+laura_user_id = "201990584"
 
 api = InstagramAPI(access_token=access_token, client_secret=client_secret)
-user_info = api.user(user_id=user_id)
+user_info = api.user(user_id=laura_user_id)
 
 print user_info.counts["followed_by"]
