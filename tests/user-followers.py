b5b85093da50e39729983d7e30cbdb4636eec650
Dan Smith <dan@bytesandbolts.com>
2015-10-23 12:18:11 +0100

    Added user followers test

---
+++ tests/user-followers.py
@@ -0,0 +1,11 @@
+from instagram.client import InstagramAPI
+import json
+
+access_token = "176201887.877f884.1cdbffd746974bb489a66c2b1df0e958"
+client_secret = "e8336639f78444919b8b3f4c1ef0c326"
+user_id = "176201887"
+
+api = InstagramAPI(access_token=access_token, client_secret=client_secret)
+user_info = api.user(user_id=user_id)
+
+print user_info.counts["followed_by"]
