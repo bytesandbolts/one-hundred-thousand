979dd3357d19f51c1856e7a5c92453b7f9a9783a
Dan Smith <dan@bytesandbolts.com>
2015-10-26 19:07:22 +0000

    Readded try catch exceptions

--- tests/watch-followers.py
+++ tests/watch-followers.py
@@ -12,9 +12,12 @@ previous_followers = 0
 api = InstagramAPI(access_token=access_token, client_secret=client_secret)
 
 while True:
-    user_info = api.user(user_id=laura_user_id)
-    number_of_followers = user_info.counts["followed_by"]
-    if number_of_followers != previous_followers:
-        print "Followers: %s - %s" % (number_of_followers, time.asctime(time.localtime(time.time())))
-        previous_followers = number_of_followers
-    time.sleep(1)
+    try:
+        user_info = api.user(user_id=laura_user_id)
+        number_of_followers = user_info.counts["followed_by"]
+        if number_of_followers != previous_followers:
+            print "Followers: %s - %s" % (number_of_followers, time.asctime(time.localtime(time.time())))
+            previous_followers = number_of_followers
+        time.sleep(1)
+    except KeyboardInterrupt:
+        sys.exit(0)
