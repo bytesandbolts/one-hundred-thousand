47b9231d19107bc98f9d1f3471b473da6c99bcaf
Dan Smith <dan@bytesandbolts.com>
2015-11-30 23:25:50 +0000

    Added stepper motor code to main code branch

--- one-hundred-thousand.py
+++ one-hundred-thousand.py
@@ -1,23 +1,65 @@
-#!/Users/dan/.virtualenvs/instagram-test/bin/python
+#!/usr/bin/env python
 from instagram.client import InstagramAPI
-import sys
-import time
+import pyupm_grove as grove
+import pyupm_uln200xa as upmULN200XA
+import sys, time
 
 access_token = "176201887.877f884.1cdbffd746974bb489a66c2b1df0e958"
 client_secret = "e8336639f78444919b8b3f4c1ef0c326"
-
-dans_user_id = "176201887"
-lauras_user_id = "201990584"
-user_id = lauras_user_id
-
+dan_user_id = "176201887"
+laura_user_id = "201990584"
 previous_followers = 0
 
+# LED setup
+green_led = grove.GroveLed(2)
+red_led = grove.GroveLed(3)
+
+# Instagram API setup
 api = InstagramAPI(access_token=access_token, client_secret=client_secret)
 
+# Stepper motor setup
+myUln200xa = upmULN200XA.ULN200XA(4096, 8, 9, 10, 11)
+myUln200xa.setSpeed(5) # 5 RPMs
+myUln200xa.setDirection(upmULN200XA.ULN200XA.DIR_CCW)
+
+# range mapper
+def translate(value, leftMin, leftMax, rightMin, rightMax):
+    # Figure out how 'wide' each range is
+    leftSpan = leftMax - leftMin
+    rightSpan = rightMax - rightMin
+
+    # Convert the left range into a 0-1 range (float)
+    valueScaled = float(value - leftMin) / float(leftSpan)
+
+    # Convert the 0-1 range into a value in the right range.
+    return rightMin + (valueScaled * rightSpan)
+
 while True:
-    user_info = api.user(user_id=user_id)
-    number_of_followers = user_info.counts["followed_by"]
-    if number_of_followers != previous_followers:
-        print "Followers: %s - %s" % (number_of_followers, time.asctime(time.localtime(time.time())))
-        previous_followers = number_of_followers
-    time.sleep(1)
+    try:
+        # Poll Instagram API
+        user_info = api.user(user_id=laura_user_id)
+
+        # Parse received data
+        number_of_followers = user_info.counts["followed_by"]
+
+        # Update display when a change is detected
+        if number_of_followers != previous_followers:
+            stepperValue = translate(number_of_followers, 0, 100000, 0, 2048)
+            print stepperValue
+            myUln200xa.stepperSteps(stepperValue)
+
+            if number_of_followers > previous_followers and previous_followers != 0:
+                green_led.on()
+            elif number_of_followers < previous_followers and previous_followers != 0:
+                red_led.on()
+            print "Followers: %s - %s" % (number_of_followers, time.asctime(time.localtime(time.time())))
+            previous_followers = number_of_followers
+
+        # Sleep for 1 second then restart event cycle
+        time.sleep(1)
+        green_led.off()
+        red_led.off()
+    except KeyboardInterrupt:
+        del green_led
+        del red_led
+        sys.exit(0)
