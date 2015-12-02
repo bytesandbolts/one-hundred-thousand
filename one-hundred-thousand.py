a584734d26c9b094f1c99ff59f6a64850184ba59
Dan Smith <dan@bytesandbolts.com>
2015-12-02 21:40:24 +0000

    Tweaked flow control more

--- one-hundred-thousand.py
+++ one-hundred-thousand.py
@@ -8,12 +8,14 @@ access_token = "176201887.877f884.1cdbffd746974bb489a66c2b1df0e958"
 client_secret = "e8336639f78444919b8b3f4c1ef0c326"
 user_id = "201990584"
 
-previous_followers = 0
+previous_number_of_followers = 0
 previous_motor_position = 0
-number_of_followers = 100000
 
-# Motor
-myUln200xa = 0
+target_followers_lower_limit = 0
+target_followers_upper_limit = 100000
+
+# Stepper Motor
+myUln200xa = None
 
 # LED setup
 green_led = grove.GroveLed(2)
@@ -41,46 +43,49 @@ while True:
 
         # Parse received data
         number_of_followers = user_info.counts["followed_by"]
-        myUln200xa = 0
+        myUln200xa = None
         # Update display when a change is detected
-        if number_of_followers != previous_followers and number_of_followers >= 0 and number_of_followers => 100000:
+        if (number_of_followers != previous_number_of_followers and 
+            number_of_followers >= target_followers_lower_limit and 
+            number_of_followers <= target_followers_upper_limit):
+            
             # LED control
-            if number_of_followers > previous_followers:
+            if (number_of_followers > previous_number_of_followers):
                 green_led.on()
-            elif number_of_followers < previous_followers:
+            elif (number_of_followers < previous_number_of_followers):
                 red_led.on()
-
+                
             # Motor position control
-            motor_position = translate(number_of_followers, 0, 100000, 0, 2048)
-            if motor_position != previous_motor_position:
+            motor_position = translate(number_of_followers, target_followers_lower_limit, target_followers_upper_limit, 0, 2048)
+            if (motor_position != previous_motor_position):
                 # Stepper motor setup
                 myUln200xa = upmULN200XA.ULN200XA(4096, 8, 9, 10, 11)
                 myUln200xa.setSpeed(5) # 5 RPMs
-
+                
                 motor_delta = motor_position - previous_motor_position
-                if motor_position > previous_motor_position:
+                if (motor_position > previous_motor_position):
                     myUln200xa.setDirection(upmULN200XA.ULN200XA.DIR_CCW)
-                elif motor_position < previous_motor_position:
+                elif (motor_position < previous_motor_position):
                     myUln200xa.setDirection(upmULN200XA.ULN200XA.DIR_CW)
-                # Rotate motor
-                # abs ensures number is always positive which is required by underlying library
+                
+                # Rotate motor - abs() ensures number is always positive which is required by underlying library
                 myUln200xa.stepperSteps(abs(motor_delta))
-                # Release otherwise motor begins to heat up quite hot.
+                # Release  motor otherwise it begins to heat up quite hot.
                 myUln200xa.release()
 
             # Print outputs
             print "Followers: %s - Motor position: %s - %s" % (number_of_followers, motor_position, time.asctime(time.localtime(time.time())))
-            previous_followers = number_of_followers
+            previous_number_of_followers = number_of_followers
             previous_motor_position = motor_position
 
         # Sleep for 1 second then restart event cycle
         time.sleep(1)
         green_led.off()
         red_led.off()
-        time.sleep(1)
     except KeyboardInterrupt:
+        # Cleanup
         del green_led
         del red_led
-        # This will error sometimes if the program exits when it is not allocated
-        myUln200xa.release()
+        if 'myUln200xa' in globals() and myUln200xa is not None:
+            myUln200xa.release()
         sys.exit(0)
